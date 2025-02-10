e70dbfe57cb478e1c0eccf233192a6893392b865
var-nan <krs.nandhan999@gmail.com>
2025-02-10 00:50:17 -0600

    refraction

--- material.h
+++ material.h
@@ -37,15 +37,44 @@ class lambertian : public material {
 class metal : public material {
     private:
         color albedo;
+        double fuzz; // fuzz reflection factor, 
+        /* this fuzz factor is used to generate a small sphere centered on the 
+         * original endpoint of the reflected ray, scaled by fuzz factor.  
+         * For the bigger fuzz sphere, the rays might scatter below the surface.
+         * and the surfact can absorb those. 
+        */
 
     public:
-        metal(const color& albedo) : albedo(albedo) {}
+        metal(const color& albedo, double fuzz_) : albedo(albedo), fuzz(std::fmin(1,fuzz_)) {}
 
         bool scatter(const ray& r_in, const hit_record& rec, 
                 color& attenuation, ray& scattered) const override {
             vec3 reflected = reflect(r_in.direction(), rec.normal);
+            reflected = unit_vector(reflected) + (fuzz * random_unit_vector());
             scattered = ray(rec.p, reflected);
             attenuation = albedo;
+            return (dot(scattered.direction(), rec.normal) > 0);
+        }
+};
+
+
+class dielectric: public material {
+    private:
+        double refraction_index;
+    
+    public:
+        dielectric(double refraction_index_) : refraction_index(refraction_index_) {}
+
+        bool scatter(const ray& r_in, const hit_record& rec,
+                color& attenuation, ray& scattered) const override {
+            
+            attenuation = color(1.0,1.0,1.0);
+            double ri = rec.front_face ? (1.0/refraction_index) : refraction_index;
+            
+            vec3 unit_direction = unit_vector(r_in.direction());
+            vec3 refracted = refract(unit_direction, rec.normal, ri);
+
+            scattered = ray(rec.p, refracted);
             return true;
         }
 };
